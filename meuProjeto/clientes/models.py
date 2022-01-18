1e3bf63ecd2d286427d630ab65bbb52701d15c1b
weverton matias <weverton@fontedeprecos.com.br>
2022-01-18 20:57:25 -0300

    Adicionando model Clientes

--- meuProjeto/clientes/models.py
+++ meuProjeto/clientes/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Clientes(models.Model):
     nome = models.CharField(max_length=120, blank=False, null=False)
     endereco = models.CharField(max_length=255, blank=False, null=False)
+    salario = models.DecimalField(max_digits=10, decimal_places=2)
+    idade = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
