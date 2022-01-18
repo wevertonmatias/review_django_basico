258e55c5d6db9fc84398cc0ae5ffc48652c7d826
weverton matias <weverton@fontedeprecos.com.br>
2022-01-18 20:32:57 -0300

    Iniciando o model Clientes

--- meuProjeto/clientes/models.py
+++ meuProjeto/clientes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Clientes(models.Model):
+    nome = models.CharField(max_length=120, blank=False, null=False)
+    endereco = models.CharField(max_length=255, blank=False, null=False)
