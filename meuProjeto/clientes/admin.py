ef03b5578a04f6a1b82d0d1e81fb19b313357c82
weverton matias <weverton@fontedeprecos.com.br>
2022-01-18 21:20:48 -0300

    Adicionando foreignkey model Telefone

--- meuProjeto/clientes/admin.py
+++ meuProjeto/clientes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Clientes
+from .models import Cliente, Telefone
 # Register your models here.
 
-admin.site.register(Clientes)
+admin.site.register(Cliente)
+admin.site.register(Telefone)
--- meuProjeto/clientes/models.py
+++ meuProjeto/clientes/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Clientes(models.Model):
+class Cliente(models.Model):
     nome = models.CharField(max_length=120, blank=False, null=False)
     endereco = models.CharField(max_length=255, blank=False, null=False)
     salario = models.DecimalField(max_digits=10, decimal_places=2)
@@ -11,3 +11,11 @@ class Clientes(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Telefone(models.Model):
+    numero = models.CharField(max_length=20)
+    descricao = models.CharField(max_length=120)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.numero
