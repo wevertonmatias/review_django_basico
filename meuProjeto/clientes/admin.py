375a416676f64dbcbfb0896f1a2f4b92fe199272
weverton matias <weverton@fontedeprecos.com.br>
2022-01-19 21:44:40 -0300

    Personalizando o admin.

--- meuProjeto/clientes/admin.py
+++ meuProjeto/clientes/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Cliente, Telefone, CPF, Dependente
 # Register your models here.
 
-admin.site.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    # fields = ['nome']
+    list_display = ['id', 'nome', 'idade', 'email']
+    list_filter = ['dependentes']
+    search_fields = ['id', 'nome', 'dependentes__nome']
+
+
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Telefone)
 admin.site.register(CPF)
 admin.site.register(Dependente)
